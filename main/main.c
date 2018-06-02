f28abbc1eb43899d641b25cddbd2079f9a317dd6
jskubick <jeff-github@skubick.com>
2018-06-02 01:14:38 -0400

    First version that appears to work!

--- main/main.c
+++ main/main.c
@@ -6,13 +6,40 @@
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 
+#include "u8g2_esp32_hal.c"
+#include <u8g2.h>
+#define SDA_PIN 5
+#define SCL_PIN 4
+#define I2C_ADDRESS 0x78
+
 esp_err_t event_handler(void *ctx, system_event_t *event)
 {
     return ESP_OK;
 }
 
+void initHal() {
+	u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
+	u8g2_esp32_hal.sda = SDA_PIN;
+	u8g2_esp32_hal.scl = SCL_PIN;
+	u8g2_esp32_hal_init(u8g2_esp32_hal);
+
+	u8g2_t u8g2;
+	u8g2_Setup_ssd1306_i2c_128x64_noname_f(&u8g2, U8G2_R0, u8g2_esp32_i2c_byte_cb, u8g2_esp32_gpio_and_delay_cb);
+	u8x8_SetI2CAddress(&u8g2.u8x8, I2C_ADDRESS);
+	u8g2_InitDisplay(&u8g2);
+	u8g2_SetPowerSave(&u8g2, 0);
+	u8g2_ClearBuffer(&u8g2);
+	u8g2_DrawBox(&u8g2, 10,20, 20, 30);
+	u8g2_SetFont(&u8g2, u8g2_font_ncenB14_tr);
+	u8g2_DrawStr(&u8g2, 0,15,"Hello World!");
+	u8g2_SendBuffer(&u8g2);
+	vTaskDelete(NULL);
+}
+
 void app_main(void)
 {
+	initHal();
+
     nvs_flash_init();
     tcpip_adapter_init();
     ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
@@ -39,4 +66,6 @@ void app_main(void)
         vTaskDelay(300 / portTICK_PERIOD_MS);
     }
 }
+
+
 
